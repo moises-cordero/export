988974b8efef269c13f8afdddace7e8e8c3cee7e
moisescordero <moises.cordero@bairesdev.com>
2022-09-29 14:13:10 -0500

    Final doc format

--- pdf_generator.py
+++ pdf_generator.py
@@ -5,64 +5,47 @@ from fpdf import FPDF
 ORIENTATION = "portrait"
 FORMAT = "letter"
 FONT_TYPE = "Arial"
-FONT_SIZE = 12
+FONT_SIZE = 10
 ALIGN = "L"
 WIDTH = 0 # Width
 HEIGHT = 6 # Height
+TITLES = {
+    'Table': 'Table/Report',
+    'Variables': 'Variables(Columns)',
+    'Universe': 'Universe used',
+    'Reason': 'Visualization reason',
+    'Data_source': 'Complete source description'
+}
 
 def create(data):
-    # fields = {"description": "Name",
-    #         "table": "Position",
-    #         "variables": ""
-    #         }
-
-    # t = "Table #2"
-    # t = data["Table #2"]
-    # data_list = []
-    # for n,row in enumerate(t["table"]):
-    #     data_list[n] = {"name": ""
-    #             }
-    #     table1_id = row[t["columns"].index("id_table1")]
-
-
     pdf = FPDF()
     pdf.add_page(ORIENTATION, FORMAT)
-    for _, tab in data.items():
+    pdf.set_font(FONT_TYPE, "B", size=FONT_SIZE+6)
+    pdf.cell(WIDTH, HEIGHT, "FRONTERS DASHBOARDS", new_x="LMARGIN", new_y="NEXT", align="C")
+    pdf.ln()
+    pdf.ln()
+    count = 1
+    for title, tab in data.items():
+        pdf.set_font(FONT_TYPE, "B", size=FONT_SIZE+4)
+        pdf.cell(WIDTH, HEIGHT, title.upper(), new_x="LMARGIN", new_y="NEXT", align=ALIGN)
+        pdf.ln()
         for key, value in tab.items():
-            if key=="name":
-                pdf.set_font(FONT_TYPE, "B", size=FONT_SIZE+4)
-                pdf.cell(WIDTH, HEIGHT, value, new_x="LMARGIN", new_y="NEXT", align=ALIGN)
-                pdf.ln()
-            else:
-                if key=="variables":
-                    value = ", ".join(value)
-                pos = ("LMARGIN", "NEXT") if key in ["description", "graph"] else ("END", "LAST")
+            if key == 'Vis':
                 pdf.set_font(FONT_TYPE, "B", size=FONT_SIZE)
-                pdf.cell(WIDTH, HEIGHT, f'{key.capitalize()}: ', new_x=pos[0], new_y=pos[1], align=ALIGN)
+                pdf.cell(WIDTH, HEIGHT, f'{count} {value}', new_x="LMARGIN", new_y="NEXT", align=ALIGN)
+            else:
+                pos = ("LMARGIN", "NEXT") if key in ["Description", "Image"] else ("END", "LAST")
+
+                if key in TITLES:
+                    pdf.set_font(FONT_TYPE, "B", size=FONT_SIZE)
+                    pdf.cell(WIDTH, HEIGHT, f'{TITLES[key]}: ', new_x=pos[0], new_y=pos[1], align=ALIGN)
+
                 pdf.set_font(FONT_TYPE, size=FONT_SIZE)
                 pdf.cell(WIDTH, HEIGHT, value, new_x="LMARGIN", new_y="NEXT", align=ALIGN)
-            pdf.ln()
+
+                pdf.ln()
         pdf.cell(WIDTH, HEIGHT, "_"*50, new_x="LMARGIN", new_y="NEXT", align=ALIGN)
         pdf.ln()
+        count += 1
 
     pdf.output("demo.pdf")
-
-
-if __name__ == '__main__':
-    data = {
-        0: {
-            "name": "Tab #1",
-            "description": "This is the description of tab #2",
-            "table": "Table of Tab #1",
-            "variables": ["var1.1", "var1.2", "var1.3"],
-            "graph": ""
-        },
-        1: {
-            "name": "Tab #2",
-            "description": "This is the description of tab #2",
-            "table": "Table of Tab #2",
-            "variables": ["var2.1", "var2.2", "var2.3"],
-            "graph": ""
-        },
-    }
-    create(data)
